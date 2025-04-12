71f3dc4f720a68299b8fb0f8687123a1968da5a3
tempura327 <cks6658324@yahoo.com.tw>
2025-04-12 22:12:55 +0800

    chore: moved css files to the style folder

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Button from '@mui/material/Button';
 
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
+import '@/styles/App.css';
 
 function App() {
   const [count, setCount] = useState(0);
