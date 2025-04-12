655de0c77aab3f5e17fe25d6526f2ea6192e40cb
tempura327 <cks6658324@yahoo.com.tw>
2025-04-12 11:48:25 +0800

    chore: formatted files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 /* eslint-env node */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   root: true,
 };
