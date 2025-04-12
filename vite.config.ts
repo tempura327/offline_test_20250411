123846eca2ca96d3c1cecc658022c00ea5da88d6
tempura327 <cks6658324@yahoo.com.tw>
2025-04-12 22:12:17 +0800

    chore: set alias for the src folder to prepare move style files to the styles folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 });
