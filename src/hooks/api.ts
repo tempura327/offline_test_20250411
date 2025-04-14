3121dc2c46efd69a4f9260a718d520a8e32f30ea
tempura327 <cks6658324@yahoo.com.tw>
2025-04-14 20:39:48 +0800

    feat: added hooks which are used to send request

---
+++ src/hooks/api.ts
@@ -0,0 +1,71 @@
+import {
+  useMutation,
+  UseMutationOptions,
+  useQuery,
+  UseQueryOptions,
+} from '@tanstack/react-query';
+import { AxiosRequestConfig } from 'axios';
+
+import { request, get, HTTPMethod } from '../utils/request';
+
+interface UseAppQueryParams<TResponse> {
+  url: string;
+  queryOption?: Omit<UseQueryOptions<TResponse>, 'queryKey'> & {
+    onSuccess?: (data: TResponse) => void;
+    onError?: (err: Error) => void;
+  };
+  axiosConfig?: AxiosRequestConfig;
+}
+
+export const useAppQuery = <TResponse>({
+  url,
+  axiosConfig,
+  queryOption,
+}: UseAppQueryParams<TResponse>) => {
+  const res = useQuery({
+    queryKey: [url],
+    queryFn: async () => {
+      return await get<TResponse>({
+        url,
+        config: axiosConfig,
+      });
+    },
+    ...queryOption,
+  });
+
+  if (res.error) {
+    queryOption?.onError?.(res.error);
+  }
+
+  if (res.data) {
+    queryOption?.onSuccess?.(res.data);
+  }
+
+  return res;
+};
+
+interface UseAppMutationParams<TResponse, TPayload> {
+  url: string;
+  method: HTTPMethod;
+  mutateOption?: UseMutationOptions<TResponse, Error, TPayload>;
+  axiosConfig?: AxiosRequestConfig;
+}
+
+export const useAppMutation = <TResponse, TPayload>({
+  url,
+  method,
+  mutateOption,
+  axiosConfig,
+}: UseAppMutationParams<TResponse, TPayload>) => {
+  return useMutation({
+    mutationFn: async (payload: TPayload) => {
+      return await request<TResponse, TPayload>({
+        url,
+        method,
+        payload,
+        config: axiosConfig,
+      });
+    },
+    ...mutateOption,
+  });
+};
---
+++ src/utils/request.ts
@@ -0,0 +1,106 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000',
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error(error);
+    return Promise.reject(error);
+  },
+);
+
+export enum HTTPMethod {
+  Post = 'post',
+  Patch = 'patch',
+  Delete = 'delete',
+  Get = 'get',
+}
+
+interface RequestParams {
+  url: string;
+  config?: AxiosRequestConfig;
+}
+
+interface RequestParamsWithPayload<TPayload> extends RequestParams {
+  payload: TPayload;
+}
+
+export const get = async <TResponse>({
+  url,
+  config,
+}: RequestParams): Promise<TResponse> => {
+  const response: AxiosResponse<TResponse> = await axiosInstance.get(
+    url,
+    config,
+  );
+
+  return response.data;
+};
+
+export const post = async <TResponse, TPayload>({
+  url,
+  payload,
+  config,
+}: RequestParamsWithPayload<TPayload>): Promise<TResponse> => {
+  const response: AxiosResponse<TResponse> = await axiosInstance.post(
+    url,
+    payload,
+    config,
+  );
+  return response.data;
+};
+
+export const patch = async <TResponse, TPayload>({
+  url,
+  payload,
+  config,
+}: RequestParamsWithPayload<TPayload>): Promise<TResponse> => {
+  const response: AxiosResponse<TResponse> = await axiosInstance.patch(
+    url,
+    payload,
+    config,
+  );
+  return response.data;
+};
+
+export const remove = async <TResponse>({
+  url,
+  config,
+}: RequestParams): Promise<TResponse> => {
+  const response: AxiosResponse<TResponse> = await axiosInstance.delete(
+    url,
+    config,
+  );
+
+  return response.data;
+};
+
+interface AppRequestParams<TPayload> {
+  url: string;
+  method: HTTPMethod;
+  config?: AxiosRequestConfig;
+  payload: TPayload;
+}
+
+export const request = async <TResponse, TPayload = object>({
+  url,
+  method,
+  payload,
+  config,
+}: AppRequestParams<TPayload>): Promise<TResponse> => {
+  switch (method) {
+    case HTTPMethod.Post:
+      return await post<TResponse, TPayload>({ url, payload, config });
+    case HTTPMethod.Patch:
+      return await patch<TResponse, TPayload>({ url, payload, config });
+    case HTTPMethod.Delete:
+      return await remove<TResponse>({ url, config });
+    default:
+      throw new Error('Invalid HTTP method');
+  }
+};
