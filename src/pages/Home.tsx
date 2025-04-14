d9fd7eefc8d6d71c6a736d37a304ecf8377d1ba3
tempura327 <cks6658324@yahoo.com.tw>
2025-04-14 22:39:52 +0800

    feat: added food to selectedFoods store and show total price on Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, ReactNode } from 'react';
+import { useMemo, ReactNode, useCallback } from 'react';
 import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
 import EmojiFoodBeverageIcon from '@mui/icons-material/EmojiFoodBeverage';
 import IcecreamIcon from '@mui/icons-material/Icecream';
@@ -6,12 +6,19 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
+import CheckIcon from '@mui/icons-material/Check';
 
 import Drawer from '../components/Drawer';
 import Counter from '../components/Counter';
 import { useAppQuery } from '../hooks/api';
 import { FoodType, Food } from '../utils/type';
 import { DAY_MILISECOND } from '../utils/constant';
+import { useAppSelector, useAppDispatch } from '../hooks/redux';
+import {
+  addSelectedFood,
+  updateSelectedFoodNumber,
+  removeSelectedFood,
+} from '../stores/selectedFoodsSlice';
 
 type DrawerItem = {
   text: string;
@@ -20,7 +27,7 @@ type DrawerItem = {
   childrenItems?: DrawerItem[];
 };
 
-const getSubDrawerItems = (text: string, action: (data: Food) => void) => {
+const getSubDrawerItem = (text: string, action: (data: Food) => void) => {
   return {
     text,
     action,
@@ -33,10 +40,17 @@ const iconMap = {
   ft_beverage: <EmojiFoodBeverageIcon />,
 };
 
-type SelectedFood = Food & { number: number };
-
 const Home = () => {
-  const [selectedFoods, setSelectedFoods] = useState<SelectedFood[]>([]);
+  const selectedFoods = useAppSelector((state) => state.selectedFoods);
+  const dispatch = useAppDispatch();
+
+  const totalPrice = useMemo(() => {
+    if (selectedFoods.length < 1) return 0;
+
+    return selectedFoods.reduce((acc, { price, number }) => {
+      return acc + price * number;
+    }, 0);
+  }, [selectedFoods]);
 
   const { data: foodTypesData } = useAppQuery<FoodType[]>({
     url: '/foodTypes',
@@ -49,14 +63,9 @@ const Home = () => {
     url: '/foods',
     queryOption: {
       staleTime: DAY_MILISECOND,
-      onSuccess: () => {
-        // TODO: 存到store
-        console.log('TODO: 存到store');
-      },
     },
   });
 
-  // TODO: data should retrieve from store
   const drawerList = useMemo(() => {
     if (!foodTypesData) return [];
 
@@ -65,17 +74,27 @@ const Home = () => {
         foodsData?.filter(({ type }) => type === currentType.id) || [];
 
       const childrenItems = targetFoods.map((food) => {
-        const actions = getSubDrawerItems(food.name, () => {
-          setSelectedFoods((prev) => [
-            ...prev,
-            {
-              ...food,
-              number: 1,
-            },
-          ]);
+        const data = getSubDrawerItem(food.name, () => {
+          const isFoodSelected = !!selectedFoods.find(
+            ({ id }) => id === food.id,
+          );
+
+          if (!isFoodSelected) {
+            dispatch(
+              addSelectedFood({
+                ...food,
+                number: 1,
+              }),
+            );
+          }
         });
 
-        return actions;
+        const isSelected = !!selectedFoods.find(({ id }) => id === food.id);
+
+        return {
+          ...data,
+          icon: isSelected ? <CheckIcon /> : null,
+        };
       });
 
       return [
@@ -89,32 +108,40 @@ const Home = () => {
     }, []);
 
     return result;
-  }, [foodTypesData, foodsData]);
+  }, [dispatch, foodTypesData, foodsData, selectedFoods]);
 
-  const handleUpdateSelectedFoods = (targetId: string, newValue: number) => {
-    setSelectedFoods((prev) => {
-      if (newValue < 1) {
-        return prev.filter(({ id }) => {
-          return id !== targetId;
+  const handleUpdateSelectedFoods = useCallback(
+    (targetData: Food, newValue: number) => {
+      const { id: targetId } = targetData;
+      const isTargetFoodSelcted = !!selectedFoods.find(
+        ({ id }) => id === targetId,
+      );
+
+      if (!isTargetFoodSelcted) {
+        const payload = addSelectedFood({
+          ...targetData,
+          number: newValue,
         });
+
+        dispatch(payload);
+
+        return;
       }
 
-      return prev.map(({ id, ...rest }) => {
-        if (id === targetId) {
-          return {
-            id,
-            ...rest,
-            number: newValue,
-          };
-        }
+      const payload =
+        newValue > 0
+          ? updateSelectedFoodNumber({
+              id: targetId,
+              newNumber: newValue,
+            })
+          : removeSelectedFood({
+              id: targetId,
+            });
 
-        return {
-          id,
-          ...rest,
-        };
-      });
-    });
-  };
+      dispatch(payload);
+    },
+    [dispatch, selectedFoods],
+  );
 
   return (
     <div className="flex h-full">
@@ -126,27 +153,33 @@ const Home = () => {
 
           <Button variant="contained">送出</Button>
         </div>
-
         <div className="my-4">
-          {selectedFoods.map(({ id, name, number }) => (
-            <div className="flex items-center [&>*+*]:ml-2" key={id}>
-              <Typography>{name}</Typography>
-              <Counter
-                value={number}
-                onValueUpdate={(newValue) => {
-                  handleUpdateSelectedFoods(id, newValue);
-                }}
-              />
-              <IconButton
-                onClick={() => {
-                  handleUpdateSelectedFoods(id, 0);
-                }}
-              >
-                <DeleteIcon />
-              </IconButton>
-            </div>
-          ))}
+          {selectedFoods.map((food) => {
+            const { id, name, number } = food;
+
+            return (
+              <div className="flex items-center [&>*+*]:ml-2" key={id}>
+                <Typography className="text-left">{name}</Typography>
+                <Counter
+                  value={number}
+                  onValueUpdate={(newValue) => {
+                    handleUpdateSelectedFoods(food, newValue);
+                  }}
+                />
+
+                <IconButton
+                  onClick={() => {
+                    handleUpdateSelectedFoods(food, 0);
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            );
+          })}
         </div>
+        <hr className="my-4" />
+        <div className="flex">總計: {totalPrice.toLocaleString()} 元</div>
       </div>
     </div>
   );
